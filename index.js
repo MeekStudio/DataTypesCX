54e7612067564d83b9febf3e05b920b2e4dc701f
Lee Bowyer <lee@meek.studio>
2023-12-01 11:54:00 +0100

    Updated Squid

--- index.js
+++ index.js
@@ -345,11 +345,39 @@ class EventLogger {
 
 class Squid {
     static name = "Squid";
+    static #syntax = /^([0-9a-fA-F]{32})$/;
     
-    constructor(){
+    constructor(){}
 
+    static test(value, {
+        syntax = this.#syntax
+    } = {}){
+        
+        const errors = [];
+        
+        if(!syntax.test(value)){
+            errors.push("SYNTAX_ERROR")
+        }
+       
+        const valid = (errors.length === 0);
+        const sanitised = (valid) ? value : null;
+        
+        return {
+            valid,
+            errors,
+            sanitised
+        };
     }
+
+    static generate(){
+       return crypto.randomUUID().replace(/-/g, "");
+    }
+
 }
+
+
+
+
 class NameSpace {
     static name = "NameSpace";
     
